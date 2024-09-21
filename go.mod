48ceccc0941a0d6e1e055b93a758c3bb64f3f5c2
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2024-09-21 18:33:46 -0400

    feat: add cors

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.23.1
 require github.com/gin-gonic/gin v1.10.0
 
 require (
+	github.com/gin-contrib/cors v1.7.2 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.7.1 // indirect
--- main.go
+++ main.go
@@ -4,12 +4,20 @@ import (
 	middlewares "nlw/pocket/middewares"
 	routes "nlw/pocket/routes"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
 	router.SetTrustedProxies(nil)
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://nlw-pocket-web-production.up.railway.app", "http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST", "DELETE", "OPTIONS"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 
 	authenticated := router.Group("/")
 
